fc3fe317c0c3fc6c268fb88e3aea93617eca42f5
2005-Erick <erickmelo0202@gmail.com>
2026-04-07 01:14:03 -0300

    Modificações finais

--- src/appconsole/Deletar.java
+++ src/appconsole/Deletar.java
@@ -26,29 +26,50 @@ public class Deletar {
 		
 		// OBS: Deletando todos os objetos do banco para retestes
 		
-		Query q = manager.query();
-		q.constrain(Artista.class);  				
-		List<Artista> resultados = q.execute();
-		for (Artista art : resultados ) {
-			manager.delete(art);
-		}
-		
-		q = manager.query();
-		q.constrain(Cidade.class);  				
-		List<Cidade> resultados2 = q.execute();
-		for (Cidade cid : resultados2 ) {
-			manager.delete(cid);
-		}
-		
-		q = manager.query();
-		q.constrain(Show.class);  				
-		List<Show> resultados3 = q.execute();
-		for (Show show : resultados3 ) {
-			manager.delete(show);
-		}
+//		Query q = manager.query();
+//		q.constrain(Artista.class);  				
+//		List<Artista> resultados = q.execute();
+//		for (Artista art : resultados ) {
+//			manager.delete(art);
+//		}
+//		
+//		q = manager.query();
+//		q.constrain(Cidade.class);  				
+//		List<Cidade> resultados2 = q.execute();
+//		for (Cidade cid : resultados2 ) {
+//			manager.delete(cid);
+//		}
+//		
+//		q = manager.query();
+//		q.constrain(Show.class);  				
+//		List<Show> resultados3 = q.execute();
+//		for (Show show : resultados3 ) {
+//			manager.delete(show);
+//		}
 		
 		// finaliza aqui
 		// ...
+		
+		
+		
+		Query q = manager.query();
+		q.constrain(Cidade.class);
+		q.descend("nome").constrain("Natal");
+		List<Cidade> cidade = q.execute();
+		Cidade c = cidade.getFirst();
+		
+		List<Show> show = c.getListaDeShow();
+		
+		if (!show.isEmpty()) {
+			for(Show s : show) {
+				manager.delete(s);
+				
+			}
+		}
+		manager.delete(c);
+		manager.commit();
+		
+		
 		
 		Util.desconectar();
 		System.out.println("\n\n aviso: feche sempre o plugin OME antes de executar aplica��o");
--- src/appconsole/Resetar.java
+++ src/appconsole/Resetar.java
@@ -1,5 +1,10 @@
 package appconsole;
 
+import java.util.List;
+
+import com.db4o.ObjectContainer;
+import com.db4o.query.Query;
+
 import modelo.Artista;
 import modelo.Cidade;
 import modelo.Show;
@@ -7,15 +12,45 @@ import util.ControleID;
 import util.Util;
 
 public class Resetar {
-    public static void main(String[] args) {
-        Util.conectar();
+	
+		private ObjectContainer manager;
+	
+	   public Resetar() {
+	        Util.conectar();
+	        manager = Util.getManager();
 
-        ControleID.resetarRegistroID(Show.class, 0);
-        ControleID.resetarRegistroID(Artista.class, 0);
-        ControleID.resetarRegistroID(Cidade.class, 0);
 
-        Util.desconectar();
+			Query q = manager.query();
+			q.constrain(Artista.class);  				
+			List<Artista> resultados = q.execute();
+			for (Artista art : resultados ) {
+				manager.delete(art);
+			}
+			
+			q = manager.query();
+			q.constrain(Cidade.class);  				
+			List<Cidade> resultados2 = q.execute();
+			for (Cidade cid : resultados2 ) {
+				manager.delete(cid);
+			}
+			
+			q = manager.query();
+			q.constrain(Show.class);  				
+			List<Show> resultados3 = q.execute();
+			for (Show show : resultados3 ) {
+				manager.delete(show);
+			}
 
-        System.out.println("IDs resetados com sucesso");
-    }
+	        ControleID.resetarRegistroID(Show.class, 0);
+	        ControleID.resetarRegistroID(Artista.class, 0);
+	        ControleID.resetarRegistroID(Cidade.class, 0);
+
+	        Util.desconectar();
+
+	        System.out.println("Banco resetado com sucesso");
+	    }
+	   
+	   public static void main(String[] args) {
+	        new Resetar();
+	    }
 }
